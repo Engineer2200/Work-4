2436d2e9dc735eeaa308e95b5c33e4d66547506f
Engineer <arsenya.suvorov@mail.ru>
2022-11-22 23:39:13 +0400

    start

--- Task23/Program.cs
+++ Task23/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+for(int i = 1; i <= number; i++)
+    {
+        Console.WriteLine(i*i*i);
+    }
