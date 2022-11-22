2619034d6d25c54934234d15c7b0064c450e00ef
Engineer <arsenya.suvorov@mail.ru>
2022-11-22 23:05:00 +0400

    start

--- Task21/Program.cs
+++ Task21/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите координату X точки A: ");
+ int Ax = Convert.ToInt32(Console.ReadeLine());
+Console.WriteLine("Введите координату Y точки A: ");
+int Ay = Convert.ToInt32(Console.ReadeLine());
+Console.WriteLine("Введите координату Z точки A: ");
+int Az = Convert.ToInt32(Console.ReadeLine());
+
+Console.WriteLine("Введите координату X точки B: ");
+int Bx = Convert.ToInt32(Console.ReadeLine());
+Console.WriteLine("Введите координату Y точки B: ");
+int Bx = Convert.ToInt32(Console.ReadeLine());
+Console.WriteLine("Введите координату Z точки B: ");
+int Bx = Convert.ToInt32(Console.ReadeLine());
+
+double distance = Math.Sqrt(Math.Pow(Bx-Ax,2)+Math.Pow(By-Ay,2)+Math.Pow(Bz-Az,2));
+Console.WriteLine(distance);
