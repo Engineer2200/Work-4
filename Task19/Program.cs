b9ff155a6fadbc316e332d7d620da7d822f6c8be
Engineer <arsenya.suvorov@mail.ru>
2022-11-22 22:41:52 +0400

    start

---
+++ Task19/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number.ToString()[0] == number.ToString()[4] && number.ToString()[1] == number.ToString()[3])
+    {
+        Console.WriteLine("Да");
+    }
+else
+    {
+        Console.WriteLine("Нет");
+    }
